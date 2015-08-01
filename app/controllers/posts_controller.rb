9df3dd01e4bb833f93c095ccf72c3fe85dbf8ef7
John Lander <john@linkodyssey.com>
2015-08-01 18:02:15 +0000

    Add devise gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,53 +1,55 @@
 class PostsController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index, :show]
     
-    def index
-      @post = Post.all.order('created_at DESC')
+  def index
+    @post = Post.all.order('created_at DESC')
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if
+      @post.save
+      flash[:success] = "Post Created!"
+      redirect_to @post
+    else
+      render 'new'
     end
-    
-    def new
-      @post = Post.new
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post Updated!"
+      redirect_to @post
+    else
+      render 'edit'
     end
-    
-    def create
-      @post = Post.new(post_params)
-      if
-        @post.save
-        flash[:success] = "Post Created!"
-        redirect_to @post
-      else
-        render 'new'
-      end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:danger] = "Post deleted!"
+    redirect_to root_path
+  end
+  
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
     end
-    
-    def show
-      @post = Post.find(params[:id])
-    end
-    
-    def edit
-      @post = Post.find(params[:id])
-    end
-    
-    def update
-      @post = Post.find(params[:id])
-      if @post.update(post_params)
-        flash[:success] = "Post Updated!"
-        redirect_to @post
-      else
-        render 'edit'
-      end
-    end
-    
-    def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      flash[:danger] = "Post deleted!"
-      redirect_to root_path
-    end
-    
-    
-    private
-      def post_params
-        params.require(:post).permit(:title, :body)
-      end
     
 end
