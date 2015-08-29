4935deaac024988d528b0bb9da233d729908cc0c
John Lander <john@linkodyssey.com>
2015-08-29 22:50:25 +0000

    Edit production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'rails-apps-johnwl.c9.io' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
 end
